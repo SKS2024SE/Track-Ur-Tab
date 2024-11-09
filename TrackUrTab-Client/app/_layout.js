e17d6eda8a1bf6c2ae87d9d54693802e79782561
KsWeasley <krithikaswami02@gmail.com>
2024-11-08 19:18:59 -0500

    Some enhancements

--- TrackUrTab-Client/app/_layout.js
+++ TrackUrTab-Client/app/_layout.js
@@ -11,9 +11,9 @@ export default function Layout() {
         <Stack.Screen name="login" />
         <Stack.Screen name="signup" />
         <Stack.Screen name="homescreen" options={{ headerShown: false }} />
-        <Stack.Screen name="addexpense" options={{ title: 'Go to Expenses Page' }} />
         <Stack.Screen name="newgroup" options={{ title: 'Go to Groups Page' }} />
         <Stack.Screen name="group-info" options={{ title: 'Group Expenses' }} />
+        <Stack.Screen name="addexpense" options={{ title: 'Go to Expenses Page' }} />
       </Stack>
     </DataProvider>
   );
--- TrackUrTab-Client/components/GroupInfo.js
+++ TrackUrTab-Client/components/GroupInfo.js
@@ -1,15 +1,29 @@
 import { View, TouchableOpacity, StyleSheet } from "react-native";
 import ExpensesRenderer from "./ExpensesRenderer";
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useData } from "../app/dataprovider";
 
 export default function GroupInfo({ data }) {
+    const { setData } = useData();
+
     console.log('Received in GroupInfo: ', data);
     return (
         <>
             <ExpensesRenderer data={data} />
             <TouchableOpacity
                 style={styles.floatingButton}
-                onPress={() => { console.log('clicked') }}
+                onPress={() => {
+                    setData({
+                        type: 'addexpense',
+                        current_user: data.current_user,
+                        expense_details: {
+                            type: 'group'
+                        },
+                        grp_id: data.user_details[data.current_user.id]?.personal_exp // Use optional chaining
+                    });
+                    console.log('Add expenses group id: ', data.user_details);
+                    router.push('/addexpense');
+                }}
             >
                 <Icon name="add-circle" size={60} color="#33e0ff" />
             </TouchableOpacity>
